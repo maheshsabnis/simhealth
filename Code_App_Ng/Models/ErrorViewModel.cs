4f198c11197181b86a475b0728a04c6bf057ae9c
maheshsabnis <sabnis_m@hotmail.com>
2022-06-17 16:55:38 +0530

    Date:17-June-2022

---
+++ Code_App_Ng/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Code_App_Ng.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
